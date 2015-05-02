09aa73b4ce0f0d7d5ef165b5ba5a6963f322f241
BazukaOC <bazukaoc@gmail.com>
2015-05-02 15:21:02 +0800

    code refactoring

--- main.cpp
+++ main.cpp
@@ -2,9 +2,10 @@
 #include <fstream>
 #include <math.h>
 #include <vector>
+
 #include "algebra3.h"
 #include "imageIO.h"
-#include "raytracer.h"
+#include "RayTracer.h"
 
 #define PI 3.1415926535898
 #define Tan(th) tan(PI/180*(th))
@@ -12,9 +13,9 @@
 
 using namespace std;
 
-vec3 eye, dir, vert, hori, light;
-float Fangle;
-int Rw, Rh;
+vec3 eye, direct, vertical = vec3(0, 1, 0), light;
+float angle;
+int resolusionW, resolusionH;
 
 float SOx, SOy, SOz, Sr;
 float Ax, Ay, Az, Bx, By, Bz, Cx, Cy, Cz;
@@ -22,15 +23,13 @@ float Ax, Ay, Az, Bx, By, Bz, Cx, Cy, Cz;
 vector<vec4> Sphere;
 vector<vec3> Triangle;
 
-vec3 intansity, checkingPoint, normal, half, reflect, refract;
-
 float Ia, Id, Is, Ii, Ke = 1000.0, Nr = 2.0;
 vec3 Ka = vec3(0.100, 0.100, 0.100);
 vec3 Kd = vec3(0.700, 0.600, 0.300);
 vec3 Ks = vec3(1.000, 1.000, 1.000);
 
 void readFile() {
-    ifstream ifile("hw1_input.txt");
+    ifstream ifile("input.txt");
     string test;
     while( ifile >> test ) {
         switch (test[0]) {
@@ -38,13 +37,13 @@ void readFile() {
                 ifile >> eye[0] >> eye[1] >> eye[2];
                 break;
             case 'V':
-                ifile >> dir[0] >> dir[1] >> dir[2];
+                ifile >> direct[0] >> direct[1] >> direct[2];
                 break;
             case 'F':
-                ifile >> Fangle;
+                ifile >> angle;
                 break;
             case 'R':
-                ifile >> Rw >> Rh;
+                ifile >> resolusionW >> resolusionH;
                 break;
             case 'S':
                 ifile >> SOx >> SOy >> SOz >> Sr;
@@ -58,96 +57,25 @@ void readFile() {
                 break;
             case 'L':
                 ifile >> light[0] >> light[1] >> light[2];
+                break;
         }
     }
-}
-
-void setRay() {
-    vert = vec3(0, 1, 0);
-    hori = vert ^ dir;
-
-    dir  =  dir.normalize();
-    vert = vert.normalize();
-    hori = hori.normalize();
-
-    hori = hori * ( dir.length() * Tan(Fangle/2) / (Rw/2) );
-    vert = vert * ( dir.length() * Tan(Fangle/2) / (Rh/2) );
-    dir  =  dir - Rw/2 * hori + Rh/2 * vert;
-    ///cout << dir[0] << ", " << dir[1] << ", " << dir[2] << endl;
-}
-
-bool isIntersectedSph(vec3 &ray, vec4 &sph, vec3 &seye) {
-
-    ///  AT^2 + BT + C = 0
-    float A = ray[0]*ray[0]+ray[1]*ray[1]+ray[2]*ray[2];
-    float B = 2*(ray[0]*(seye[0]-sph[0])+ray[1]*(seye[1]-sph[1])+ray[2]*(seye[2]-sph[2]));
-    float C = (seye[0]-sph[0])*(seye[0]-sph[0])+(seye[1]-sph[1])*(seye[1]-sph[1])+(seye[2]-sph[2])*(seye[2]-sph[2])-sph[3]*sph[3];
-
-    if ( ( B*B - 4*A*C ) >= 0 ) {
-        ///  T = ( -B +- (B^2 - 4*C)^1/2 ) / 2
-        float t = ( -B - sqrt(B*B - 4*A*C) ) / 2;
-        checkingPoint = vec3(seye[0] + t*ray[0], seye[1] + t*ray[1], seye[2] + t*ray[2]);
-        normal = checkingPoint - vec3(sph[0], sph[1], sph[2]);
-        normal = normal.normalize();
-        return true;
-    }
-    return false;
-}
-
-bool isIntersectedTri(vec3 &ray, vec3 &a, vec3 &b, vec3 &c, vec3 &seye) {
-
-    vec3 S1 = b - a;
-    vec3 S2 = c - a;
-    normal  = S1 ^ S2;
-
-    if (normal * ray > 0) { normal = normal * -1; }
-
-    normal = normal.normalize();
-
-    /// there is no triangle
-    if( normal == vec3(0, 0, 0) ) { return false; }
-
-    /// ray is not in the triangle plane
-    if( ray * normal == 0 ) { return false; }
-
-    /// ray is away from the triangle, ax + by + cz = d
-    float d = normal[0]*a[0] + normal[1]*a[1] + normal[2]*a[2];
-    float t = ( d - ( normal[0]*seye[0] + normal[1]*seye[1] + normal[2]*seye[2] ) ) / ( normal[0]*ray[0] + normal[1]*ray[1] + normal[2]*ray[2] );
-    if ( t < 0.0 ) { return false; }
-
-    /// inside test
-    float inaccuracy = 0.0001;
-    checkingPoint = vec3(seye[0] + t*ray[0], seye[1] + t*ray[1], seye[2] + t*ray[2]);
-
-    vec3 S3 = checkingPoint - a;
-    vec3 S4 = b - checkingPoint;
-    vec3 S5 = c - checkingPoint;
-
-    vec3 v31 = S1 ^ S3; float tri31 = v31.length() / 2;
-    vec3 v32 = S2 ^ S3; float tri32 = v32.length() / 2;
-    vec3 v12 = S1 ^ S2; float tri12 = v12.length() / 2;
-    vec3 v45 = S4 ^ S5; float tri45 = v45.length() / 2;
-
-    //if ( tri31 + tri32 + tri45 - tri12 <= inaccuracy ) { return true;}
-
-    //if ( (tri31 + tri32) > tri12 ) { return false; }
-    if ( (tri31 + tri32 + tri45 - tri12) > inaccuracy ) { return false; }
-
-    return true;
 }
 
 int main()
 {
     readFile();
+    RayTracer raytracer(eye, direct, vertical, angle, resolusionW, resolusionH);
     ColorImage image;
-    image.init(Rw, Rh);
-    Pixel white = {255, 255, 255};
-    for(int x = 0; x < Rw; x += 1) {
-        for(int y = 0; y < Rh; y += 1) {
-            image.writePixel(x, y, white);
+    image.init(resolusionW, resolusionH);
+    for(int x = 0; x < resolusionW; x += 1) {
+        for(int y = 0; y < resolusionH; y += 1) {
+
+            Pixel color;
+            image.writePixel(x, y, color);
         }
     }
-    char outputname[15] = "output.ppm";
+    char outputname[11] = "output.ppm";
     image.outputPPM(outputname);
     return 0;
 }
--- raytracer.h
+++ raytracer.h
@@ -1,16 +1,21 @@
 #ifndef RAYTRACER_H_INCLUDED
 #define RAYTRACER_H_INCLUDED
 
+#include <vector>
 #include <math.h>
 #include "algebra3.h"
+
+#define PI 3.1415926535898
+#define Tan(th) tan(PI/180*(th))
+#define Cos(th) cos(PI/180*(th))
 
 typedef vec3 ambient;
 typedef vec3 diffuse;
 typedef vec3 speculr;
 
-class raytracer {
+class RayTracer {
 public:
-    raytracer(vec3 start, vec3 direct, vec3 vertical, int angle, int resolutionW, int resolutionH) {
+    RayTracer(vec3 start, vec3 direct, vec3 vertical, int angle, int resolutionW, int resolutionH) {
         start_       = start;
         direct_      = direct;
         vertical_    = vertical;
@@ -18,94 +23,113 @@ public:
         resolutionW_ = resolutionW;
         resolutionH_ = resolutionH;
 
-        horizon_  = vertical_ ^ direct_;
-        direct_   = direct_.normalize();
-        vertical_ = vertical_.normalize();
-        horizon_  = horizon_.normalize();
+        horizon_     = vertical_ ^ direct_;
+        direct_      = direct_.normalize();
+        vertical_    = vertical_.normalize();
+        horizon_     = horizon_.normalize();
+
+        horizonU_    = horizon_  * ( direct_.length() * Tan(angle_/2) / (resolutionW_/2) );
+        verticalU_   = vertical_ * ( direct_.length() * Tan(angle_/2) / (resolutionH_/2) );
+        directU_     = direct_ - resolutionW_/2 * horizon_ + resolutionH_/2 * vertical_;
     }
-    raytracer(vec3 start, vec3 direct, int count) {
+    RayTracer(vec3 start, vec3 direct, int count) {
         start_  = start;
         direct_ = direct;
         count_  = count;
     }
-    bool isIntersectedSphere(vec4);      /// not done, must return distance to check the first impact object
-    bool isIntersectedTriangle(vec3[3]); /// not done, must return distance to check the first impact object
+    bool isIntersectedSphere(std::vector<vec4>&);      /// not done, must return distance to check the first impact object
+    bool isIntersectedTriangle(std::vector<vec3>&);    /// not done, must return distance to check the first impact object
     vec3 firstRayGenerator(int, int);
     vec3 raytracing(vec3, vec3, int);
 private:
-    int  count_, color_[3];
+    int  count_;
     int  angle_, resolutionW_, resolutionH_;
     vec3 start_, direct_, horizon_, vertical_;
+    vec3 directU_, horizonU_, verticalU_;
     vec3 ka_, kd_, ks_, intanse_;
+    double distance_, exponential_;
+    vec3 collectColor_;
 };
 
-bool raytracer::isIntersectedSphere(vec4 sphere) {
-    ///  AT^2 + BT + C = 0
-    double A = direct_[0]*direct_[0]+direct_[1]*direct_[1]+direct_[2]*direct_[2];
-    double B = 2*(direct_[0]*(start_[0]-sphere[0])+direct_[1]*(start_[1]-sphere[1])+direct_[2]*(start_[2]-sphere[2]));
-    double C = (start_[0]-sphere[0])*(start_[0]-sphere[0])+(start_[1]-sphere[1])*(start_[1]-sphere[1])+(start_[2]-sphere[2])*(start_[2]-sphere[2])-sphere[3]*sphere[3];
+bool RayTracer::isIntersectedSphere(std::vector<vec4> &sphere) {
+    for(std::size_t i = 0; i < sphere.size(); i += 1) {
+        double A = direct_[0]*direct_[0]+direct_[1]*direct_[1]+direct_[2]*direct_[2];
+        double B = 2*(direct_[0]*(start_[0]-sphere[i][0])+direct_[1]*(start_[1]-sphere[i][1])+direct_[2]*(start_[2]-sphere[i][2]));
+        double C = (start_[0]-sphere[i][0])*(start_[0]-sphere[i][0])+(start_[1]-sphere[i][1])*(start_[1]-sphere[i][1])+(start_[2]-sphere[i][2])*(start_[2]-sphere[i][2])-sphere[i][3]*sphere[i][3];
 
-    if ( ( B*B - 4*A*C ) >= 0 ) {
-        ///  T = ( -B +- (B^2 - 4*C)^1/2 ) / 2
-//        float t = ( -B - sqrt(B*B - 4*A*C) ) / 2;
-//        checkingPoint = vec3(start_[0] + t*direct_[0], start_[1] + t*direct_[1], start_[2] + t*direct_[2]);
-//        normal = checkingPoint - vec3(sphere[0], sphere[1], sphere[2]);
-//        normal = normal.normalize();
+        if ( ( B*B - 4*A*C ) >= 0 ) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool RayTracer::isIntersectedTriangle(std::vector<vec3> &triangle) {
+    for(std::size_t i = 0; i < triangle.size()/3; i += 1) {
+        vec3 S1 = triangle[3*i+1] - triangle[3*i];
+        vec3 S2 = triangle[3*i+2] - triangle[3*i];
+        vec3 normal  = S1 ^ S2;
+
+        if (normal * direct_ > 0) { normal = normal * -1; }
+
+        normal = normal.normalize();
+
+        /// there is no triangle
+        if( normal == vec3(0, 0, 0) ) { return false; }
+
+        /// ray is not in the triangle plane
+        if( direct_ * normal == 0 ) { return false; }
+
+        /// ray is away from the triangle, ax + by + cz = d
+        double d = normal[0]*triangle[3*i][0] + normal[1]*triangle[3*i][1] + normal[2]*triangle[3*i][2];
+        double t = ( d - ( normal[0]*start_[0] + normal[1]*start_[1] + normal[2]*start_[2] ) ) / ( normal[0]*direct_[0] + normal[1]*direct_[1] + normal[2]*direct_[2] );
+        if ( t < 0.0 ) { return false; }
+
+        /// inside test
+        double inaccuracy = 0.0001;
+        vec3 checkingPoint = vec3(start_[0] + t*direct_[0], start_[1] + t*direct_[1], start_[2] + t*direct_[2]);
+
+        vec3 S3 = checkingPoint - triangle[3*i];
+        vec3 S4 = triangle[3*i+1] - checkingPoint;
+        vec3 S5 = triangle[3*i+2] - checkingPoint;
+
+        vec3 v31 = S1 ^ S3; double tri31 = v31.length() / 2;
+        vec3 v32 = S2 ^ S3; double tri32 = v32.length() / 2;
+        vec3 v12 = S1 ^ S2; double tri12 = v12.length() / 2;
+        vec3 v45 = S4 ^ S5; double tri45 = v45.length() / 2;
+
+        if ( (tri31 + tri32 + tri45 - tri12) > inaccuracy ) { return false; }
         return true;
     }
     return false;
 }
 
-bool raytracer::isIntersectedTriangle(vec3 tri[3]) {
-
-    vec3 S1 = tri[1] - tri[0];
-    vec3 S2 = tri[2] - tri[0];
-    vec3 normal  = S1 ^ S2;
-
-    if (normal * direct_ > 0) { normal = normal * -1; }
-
-    normal = normal.normalize();
-
-    /// there is no triangle
-    if( normal == vec3(0, 0, 0) ) { return false; }
-
-    /// ray is not in the triangle plane
-    if( direct_ * normal == 0 ) { return false; }
-
-    /// ray is away from the triangle, ax + by + cz = d
-    double d = normal[0]*tri[0][0] + normal[1]*tri[0][1] + normal[2]*tri[0][2];
-    double t = ( d - ( normal[0]*start_[0] + normal[1]*start_[1] + normal[2]*start_[2] ) ) / ( normal[0]*direct_[0] + normal[1]*direct_[1] + normal[2]*direct_[2] );
-    if ( t < 0.0 ) { return false; }
-
-    /// inside test
-    double inaccuracy = 0.0001;
-    vec3 checkingPoint = vec3(start_[0] + t*direct_[0], start_[1] + t*direct_[1], start_[2] + t*direct_[2]);
-
-    vec3 S3 = checkingPoint - tri[0];
-    vec3 S4 = tri[1] - checkingPoint;
-    vec3 S5 = tri[2] - checkingPoint;
-
-    vec3 v31 = S1 ^ S3; double tri31 = v31.length() / 2;
-    vec3 v32 = S2 ^ S3; double tri32 = v32.length() / 2;
-    vec3 v12 = S1 ^ S2; double tri12 = v12.length() / 2;
-    vec3 v45 = S4 ^ S5; double tri45 = v45.length() / 2;
-
-    if ( (tri31 + tri32 + tri45 - tri12) > inaccuracy ) { return false; }
-
-    return true;
+vec3 RayTracer::firstRayGenerator(int x, int y) {
+    vec3 firstRay = directU_ + x * horizonU_ - y * verticalU_;
+    firstRay = firstRay.normalize();
+    return firstRay;
 }
 
-vec3 raytracer::firstRayGenerator(int x, int y) {
-    /// return the Color
-    return vec3(0, 0, 0);
-}
-
-vec3 raytracer::raytracing(vec3 start, vec3 direct, int count) {
+vec3 RayTracer::raytracing(vec3 start, vec3 direct, int count) {
     if(count == 0) {
         return vec3(0, 0, 0);
     } else {
         return raytracing(start, direct, count - 1);
     }
 }
+
+class Ray {
+public:
+    Ray(vec3 start, vec3 direct) {
+        start_  = start;
+        direct_ = direct;
+    }
+    vec3 start()  {return start_;}
+    vec3 direct() {return direct_;}
+private:
+    vec3 start_;
+    vec3 direct_;
+};
+
 
 #endif // RAYTRACER_H_INCLUDED
